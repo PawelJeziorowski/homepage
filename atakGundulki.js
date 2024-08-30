3fc2555dd171393a982fec20284b45db1134adaa
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-08-30 18:29:29 +0200

    add Gundula attak

---
+++ atakGundulki.js
@@ -0,0 +1,6 @@
+const lastName = "Zostałaś/eś zaatakowana przez Gundulkę!";
+const age = 35;
+alert("Atak Gundulki!");
+console.log("Gundulka zaatakowała");
+console.log(lastName);
+console.log(`${lastName} i zostało ci ${age} lat życia`);

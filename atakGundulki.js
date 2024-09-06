6d6a80a401f2ff75780a5834b8cbdc9a35819f11
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-09-06 19:46:14 +0200

    add BAM, padding ect

--- atakGundulki.js
+++ atakGundulki.js
@@ -4,3 +4,8 @@ alert("Atak Gundulki!");
 console.log("Gundulka zaatakowała");
 console.log(lastName);
 console.log(`${lastName} i zostało ci ${age} lat życia`);
+let a = document.querySelector('h1');
+console.log(a);
+
+let b =document.querySelector('.ala');
+b.innerHTML= 'Gundulka jest wieczna!'

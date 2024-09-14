d9f40e0d2d6fca9f7e5a055f2a764330c0a2ec1f
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-09-14 11:36:45 +0200

    backup

--- atakGundulki.js
+++ atakGundulki.js
@@ -9,3 +9,9 @@ console.log(a);
 
 let b =document.querySelector('.ala');
 b.innerHTML= 'Gundulka jest wieczna!'
+let name ="Leon";
+let age1 = 33;
+function welcome (a,b){
+    console.log("Witaj "+ a + " masz " + b + " lata")
+}
+welcome(name, age1);
